8f7b0b747f8f7a90129b6d120a5db7e04b5ae9f8
kyushun <27068326+kyushun@users.noreply.github.com>
2020-10-09 16:16:27 +0900

    twitter module をリファクタリング

--- twitter.py
+++ twitter.py
@@ -17,10 +17,27 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-def get_image_urls(id):
-    status = api.get_status(id)
+def get_status(id):
+    return api.get_status(id)
+
+def get_url(status):
+    return f"https://twitter.com/{status.user.id}/status/{status.id}"
+
+def get_username(status):
+    return status.user.name
+
+def get_post_date(status):
+    return status.created_at
+
+def get_text(status):
+    return status.text
+
+def get_image_urls(status):
     image_urls = []
-    for media in status.extended_entities["media"]:
-        image_urls.append(media["media_url_https"])
-    image_urls = [url + ":orig" for url in image_urls]
-    return image_urls
+    if hasattr(status, "extended_entities") and "media" in status.extended_entities:
+        for media in status.extended_entities["media"]:
+            image_urls.append(media["media_url_https"])
+        image_urls = [url + ":orig" for url in image_urls]
+        return image_urls
+    else:
+        return []
